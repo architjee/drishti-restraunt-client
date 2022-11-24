fb8efa223d082e8d75c452614ae00f6ddb16826c
Archit Jain <32292295+architjee@users.noreply.github.com>
2022-11-24 12:34:51 +0530

    SummaryDashboard updated

--- src/views/SummaryDashboard.vue
+++ src/views/SummaryDashboard.vue
@@ -2,26 +2,26 @@
   <div v-if="loading">Loading</div>
   Hi! Welcome to the
   Summary Dashboard
-  <a-table :pagination="{pageSize:6}" :data-source="dishData" :columns="columns">
+  <a-table :pagination="{pageSize:6}" :data-source="currentStateOfData" :key="currentStateOfData" :columns="columns">
     <template #headerCell="{ column }">
       <template v-if="column.key === 'name'">
         <span style="color: #1890ff">Name</span>
       </template>
     </template>
-    <template #customFilterDropdown="{ setSelectedKeys, selectedKeys, confirm, clearFilters, column }">
+    <template class="a-input-tag" #customFilterDropdown="{ setSelectedKeys, selectedKeys, confirm, clearFilters, column }">
       <div style="padding: 8px">
         <a-input ref="searchInput" :placeholder="`Search ${column.dataIndex}`" :value="selectedKeys[0]"
           style="width: 188px; margin-bottom: 8px; display: block"
           @change="e => setSelectedKeys(e.target.value ? [e.target.value] : [])"
-          @pressEnter="handleSearch(selectedKeys, confirm, column.dataIndex)" />
+          @pressEnter="handleSearch(selectedKeys, confirm, column.dataIndex)" class="a-input-tag"/>
         <a-button type="primary" size="small" style="width: 90px; margin-right: 8px"
-          @click="handleSearch(selectedKeys, confirm, column.dataIndex)">
+          @click="handleSearch(selectedKeys, confirm, column.dataIndex)" >
           <template #icon>
             <SearchOutlined />
           </template>
           Search
         </a-button>
-        <a-button size="small" style="width: 90px" @click="handleReset(clearFilters)">
+        <a-button size="small" style="width: 90px" @click="handleReset()">
           Reset
         </a-button>
       </div>
@@ -41,8 +41,7 @@
     </template>
     
   </a-table>
-  {{ dishData }}
-  {{ state }}
+
 </template>
 
 <style>
@@ -57,29 +56,29 @@ export default {
   components: {
     SearchOutlined
   },
+  computed:{
+      // a computed getter
+      getCurrentStateOfData() {
+      // `this` points to the component instance
+      return this.currentStateOfData
+    }
+  },
   data() {
     return {
       loading: false,
       db: getFirestore(firebaseapp),
       dishData: [],
-      
+      currentStateOfData : [],
       state: {
         searchText: '',
         searchedColumn: '',
       },
-      searchInput: {},
       columns: [
         {
           title: 'Dish Name',
           dataIndex: 'dish',
           customFilterDropdown: true,
           onFilter: (value, record) =>record.dish.toString().toLowerCase().includes(value.toLowerCase()),
-          // onFilterDropdownVisibleChange: visible => {
-          // if (visible) {
-          //   setTimeout(() => {
-          //     this.searchInput.value.focus();
-          //   }, 100);
-          // }
         },
         {
           title: 'Station',
@@ -87,13 +86,7 @@ export default {
           customFilterDropdown: true,
           onFilter: (value, record) =>
           record.station.toString().toLowerCase().includes(value.toLowerCase()),
-        //   onFilterDropdownVisibleChange: visible => {
-        //   if (visible) {
-        //     setTimeout(() => {
-        //       searchInput.value.focus();
-        //     }, 100);
-        //   }
-        // },
+
         },
         {
           title: 'Duration',
@@ -119,19 +112,34 @@ export default {
         // },
         }
       ],
+      searchInput: {
+        dish: false,
+        station: false,
+        action: false,
+      },
+      
 
     };
   },
   methods: {
      handleSearch(selectedKeys, confirm, dataIndex) {
-      // We need to handle our sorting somehow
+      console.log(selectedKeys)
+      this.currentStateOfData=this.currentStateOfData.filter(record=>{
+        let text = record[dataIndex]  
+        if(text==null || text==undefined)
+        {
+          return false
+        }
+    return text.toString().toLowerCase().includes(selectedKeys[0].trim().toLowerCase());
+  });
       confirm();
+      this.searchInput[selectedKeys]=true
       this.state.searchText = selectedKeys[0];
       this.state.searchedColumn = dataIndex;
     },
-    handleReset (clearFilters) {
-      console.log("clearFilters", clearFilters)
-      clearFilters({ confirm: true });
+    handleReset () {
+      console.log("Force resetting by rereading the data from db");
+      this.readData();
       this.state.searchText = '';
     },
     async readData() {
@@ -145,25 +153,31 @@ export default {
       });
       console.log(dishData)
       this.dishData = dishData
+      this.currentStateOfData = this.dishData;
       this.loading = false;
     },
     async addDoc() {
-      try {
-        const docRef = await addDoc(collection(this.db, "data-collection"), {
-          dish: "Ada",
-          startTime: "24 Nov 8:21AM",
-          duration: 10
-        });
-        console.log("Document written with ID: ", docRef.id);
-      } catch (e) {
-        console.error("Error adding document: ", e);
-      }
+      // This is a dummy function to add sample documents.
+      // try {
+      //   const docRef = await addDoc(collection(this.db, "data-collection"), {
+      //     dish: "Ada",
+      //     startTime: "24 Nov 8:21AM",
+      //     duration: 10
+      //   });
+      //   console.log("Document written with ID: ", docRef.id);
+      // } catch (e) {
+      //   console.error("Error adding document: ", e);
+      // }
     }
   },
   mounted() {
     this.readData()
+
   },
 
 };
 
 </script>
+<style scoped>
+
+</style>
