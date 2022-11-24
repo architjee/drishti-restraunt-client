59522e017aea7db548fe73598724ace592afabc0
Archit Jain <32292295+architjee@users.noreply.github.com>
2022-11-24 08:53:07 +0530

    Adding a temp. placeholder for loading notification

--- src/views/SummaryDashboard.vue
+++ src/views/SummaryDashboard.vue
@@ -1,4 +1,5 @@
 <template>
+  <div v-if="loading">Loading</div>
   Hi! Welcome to the 
   Summary Dashboard
 </template>
@@ -13,17 +14,21 @@ import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore";
 export default {
   data() {
     return {
+      loading: false,
       db: getFirestore(firebaseapp),
     };
   },
   methods: {
     async readData() {
+      this.loading = true
       let dishData = [];
       const querySnapshot = await getDocs(collection(this.db, "data-collection"));
       console.log("We got the following querySnapshot", querySnapshot);
       querySnapshot.forEach((doc) => {
         console.log(`${doc.id} => ${doc.data()}`);
       });
+
+      this.loading = false;
     },
     async addDoc() {
       try {
